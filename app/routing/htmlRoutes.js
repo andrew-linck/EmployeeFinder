c55a702c3a445d3beb83a0a05a462feb59a1e0f6
Andrew Linck <alinck@purdue.edu>
2019-02-15 18:56:49 -0500

    all

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = function(app) { 
-    app.get('/', function(req, res) {
+    app.get('https://employee-find-someone-similar.herokuapp.com/', function(req, res) {
         res.sendFile(path.join(__dirname, '../public', 'home.html'));
     });
 
-    app.get('/survey', function(req, res) {
+    app.get('https://employee-find-someone-similar.herokuapp.com/survey', function(req, res) {
         res.sendFile(path.join(__dirname, '../public', 'survey.html'));
     });
 }
