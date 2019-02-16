254902abf84fc9878283a868331acad1bac36ed8
Andrew Linck <alinck@purdue.edu>
2019-02-15 21:06:03 -0500

    master

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -22,28 +22,31 @@ module.exports = function (app) {
 
         // This variable will calculate the difference between the user's scores and the scores of
         // each user in the employees data
-        let lowestDifference=100;
+        let lowestDifference=100; // 10
         let employeeRating=100;
         let employeeMatch;
         let employeeSum = 0;
 
 
         // Here we loop through all the employee possibilities in the employees data.
-        // Logic courtesy off jim-walker on github. I struggled to come up with a solution on my own, so I used his.
+        // Logic courtesy of jim-walker on github. I struggled to come up with a solution on my own, so I used his.
+
+        // I did have to use my own pass through for reducing
 
         for (let i = 0; i < employees.length; i++) {
-            employeeSum = 0;
+            employeeSum = 0; // Goes through array and for each scores it reduces and makes a sum out of them then the rating is oru sum - their sum.
             employeeSum = employees[i].scores.reduce(reducer);
             employeeRating =  Math.abs(parseInt(userSum) - parseInt(employeeSum));
 
-            // This is the part that I stumbled on. I realized I needed to make my own variable and then have a function check if 
+            // When looping through, this sets the employeeMatch to the most recent loop index if the newest employeeRating is lower than the previous. Then the employeeMatch number(which is an index) is used in best match to find the correct employee.
             if (employeeRating<=lowestDifference){
                 lowestDifference=employeeRating;
                 employeeMatch=i;
             }
         }
-
+        
         const bestMatch = {
+            // employeeMatch is from the if statement up above.
             'name': employees[employeeMatch].name,
             'photo': employees[employeeMatch].photo,
             'difference': employeeRating
